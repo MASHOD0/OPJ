80ad921fab9c133b5ae1c0a2dfeafc1d25cc10c1
MASHOD0 <mashhood2002@gmail.com>
2021-11-13 10:50:26 +0530

    init

---
+++ Theory/access_control/Bank_Main.java
@@ -0,0 +1,21 @@
+/** Bank access control */
+
+
+
+class Bank_details{
+	public string name;
+	public string address;
+	private int account_number;
+
+	void get_acc_no(int a){
+		a = account_number;
+	}
+}
+class Bank_Main{
+	public static void main(String arg[]){
+		Bank_details person1 = new Bank_details();
+		person1.name = "first person";
+		person1.address = "address 1";
+		person1.account_number = 1234;
+	}
+}
