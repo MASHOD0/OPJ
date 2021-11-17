c526ad7ead3209887c0283a8b10a04bb670ceaae
MASHOD0 <mashhood2002@gmail.com>
2021-11-17 09:55:04 +0530

    Object_incr.java

---
+++ Theory/static_keyword/Object_incr.java
@@ -0,0 +1,15 @@
+class Object {
+	static int count = 0;
+	void counter(){
+		count++;
+		System.out.println(count);
+	}
+}
+class Object_main{
+
+	public static void main( String arg[]){
+		Object c1 = new Object();
+		Object c2 = new Object();
+	}
+}
+
