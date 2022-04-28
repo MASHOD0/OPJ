c98c662089813952ec91a38793cb993f61168a89
MASHOD0 <mashhood2002@gmail.com>
2022-04-28 16:51:20 +0530

    add main

--- Theory/class_objects/Main.java
+++ Theory/class_objects/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String arg[]){
         int num = Integer.parseInt(arg[0]);
-        System.out.println(num);
+        // System.out.println(num);
         int factorial = 1;
         while(num != 0){
             factorial *= num;
