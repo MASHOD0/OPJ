a4fdaf0493aba5d0ced355e04acbfcedcac8aedc
MASHOD0 <mashhood2002@gmail.com>
2022-04-28 14:59:46 +0530

    add Swap

--- Theory/Swap.java
+++ Theory/Swap.java
@@ -2,15 +2,22 @@ class Swappy
 {
     int a;
     int b;
-  Swappy(int a,int b)
-  {
+  Swappy(int a,int b){
       this.a=a;
       this.b=b;
-
   }
-  Swappy(Swappy obj)
-  {
- b=obj.b;
- a=obj.a;
+  Swappy swap(Swappy s){
+      int temp=s.a;
+      s.a=s.b;
+      s.b=temp;
+      return s;
+  }
 }
+class Swap{
+    public static void main(String[] args) {
+        Swappy s = new Swappy(10, 20);
+        System.out.println("Before Swap: " + s.a + " " + s.b);
+        s=s.swap(s);
+        System.out.println("After Swap: " + s.a + " " + s.b);
+    }
 }
